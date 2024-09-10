075b11ca5b61d18dd3554940e92084cd9dde82e3
svetikbaihe <166598440+svetikbaihe@users.noreply.github.com>
2024-09-10 11:55:14 +0900

    Feat/typography (#2)
    * Set headings' size
    
    * feat:typography
    
    ---------
    
    Co-authored-by: Світлана Ем <svitlanaem@MacBook-Air-Svitlana.local>

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React, { StrictMode } from "react";
+import { ConfigProvider } from "antd";
+import theme from '../theme';
 import './App.scss';
 
 const App: React.FC = () => {
 
   return (
     <StrictMode>
+      <ConfigProvider theme={theme}>
+        
+      </ConfigProvider>
     </StrictMode>
   )
 }
